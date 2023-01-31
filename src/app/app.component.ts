f3ed6823c15ff7c69b93e440157c7c6c4ce82936
farzeena123 <farzeena2000@gmail.com>
2023-01-31 12:51:37 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import{Todo}from './todo';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(1000)),
+    ]),
+
+  ]
 })
 export class AppComponent {
-  title = 'my-angular';
+  title = 'ANG';
+todoValue:string;
+list:Todo[];
+
+ngOnInit(){
+this.list=[];
+this.todoValue="";
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage = "\o/";
+  console.log("cond");
+  e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
+  return confirmationMessage;              // Gecko, WebKit, Chrome <34
+});
 }
+
+addItem(){
+if(this.todoValue !== ""){
+const newItem:Todo={
+id:Date.now(),
+value:this.todoValue,
+isDone:false
+};
+this.list.push(newItem);
+}
+this.todoValue="";
+}
+
+deleteItem(id:number){
+this.list=this.list.filter(item=>item.id!== id);
+}
+
+}
+
